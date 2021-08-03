9a7222edea058ae44195f3cfee0867023ec4e16c
vishal1177 <66308507+vishal1177@users.noreply.github.com>
2021-08-03 23:26:39 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 
 public class Hello {
-public static void main(String... s) {
+public static void main(String[] s) {
 	System.out.println("vishal upadhyay");
 	System.out.println("vishal upadhyay");
 }
